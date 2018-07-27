0f0d9c7a411f291d090fac1b11082ed5a99a02f4
SnowyOwl <wade_ju@me.com>
2018-07-27 00:19:43 -0700

    add navigator

--- ShellScripts/test_navigation.sh
+++ ShellScripts/test_navigation.sh
@@ -2,16 +2,11 @@
 # find where the directory is
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 export TURTLEBOT_GAZEBO_WORLD_FILE=$DIR/../World/test.world
-#export ROBOT_INITIAL_POSE="-x 1 -y 1"
 # launch turtlebot Gazebo with test world
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-# gmapping
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" &
+# launch ACML
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 # launch rviz
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 5
-# launch wall follower 
-xterm  -e  " rosrun wall_follower wall_follower" &
-#sleep 5
