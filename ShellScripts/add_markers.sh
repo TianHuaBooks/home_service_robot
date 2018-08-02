b712e3e6b93581182fdde4d9e0255ba54da47948
SnowyOwl <wade_ju@me.com>
2018-08-02 00:04:59 -0700

    add markers

--- ShellScripts/add_markers.sh
+++ ShellScripts/add_markers.sh
@@ -6,12 +6,12 @@ export TURTLEBOT_GAZEBO_WORLD_FILE=$DIR/../World/test.world
 # launch turtlebot Gazebo with test world
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-# launch AMCL with a map file
+# AMCL
 xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$DIR/../World/map.yaml"&
 sleep 5
 # launch rviz
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-# launch pick objects 
-xterm  -e  " rosrun pick_objects pick_objects" &
+# launch add markers 
+#xterm  -e  " rosrun add_markers add_markers" &
 sleep 5
